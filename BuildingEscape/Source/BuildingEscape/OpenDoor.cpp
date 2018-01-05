0285cff8c21063c2f760236ce81dafaf62b05bf8
u5162641-Alex <u5162641@anu.edu.au>
2018-01-06 10:40:02 +1100

    First Room Built

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,44 @@
+// Copyright Alex Stuchbery 2018
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find the owning actor
+	AActor* Owner=  GetOwner();
+
+	// Create a rotator
+	FRotator NewRotation = FRotator(0.f, 90.f, 0.f);
+
+	// set the door rotation
+	Owner->SetActorRotation(NewRotation);
+	
+	
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
